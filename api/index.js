aa5bede36c8116af18831af23ae3a1e306bc34a5
Muhummad-Sami <samirao9372@gmail.com>
2026-06-20 08:23:13 +0500

    upadates

--- api/index.js
+++ api/index.js
@@ -1,10 +1,2 @@
-import app from "../backend/server.js";
-
-export default async function handler(req, res) {
-  await new Promise((resolve, reject) => {
-    app(req, res, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
-}
+import app from "../server.js";
+export default app;
