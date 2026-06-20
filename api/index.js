46d321ce71fbb5726efd6388f64ababb58f52d82
Muhummad-Sami <samirao9372@gmail.com>
2026-06-20 08:36:29 +0500

    add new files

--- api/index.js
+++ api/index.js
@@ -1,2 +1,10 @@
-import app from "../server.js";
-export default app;
+import app from "../backend/server.js";
+
+export default async function handler(req, res) {
+  await new Promise((resolve, reject) => {
+    app(req, res, (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
